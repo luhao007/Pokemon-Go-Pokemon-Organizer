356f647cf461d6b3a9b9e55f2da452bf939a1012
luhao007 <luhao007@gmail.com>
2025-11-24 20:40:53 +0900

    Updated game master, with some code type hint

--- game_master.py
+++ game_master.py
@@ -1,42 +1,46 @@
-import urllib.request
 import json
+import urllib.request
+from typing import Any
 
-VERSION_FILE = 'https://raw.githubusercontent.com/PokeMiners/game_masters/master/latest/timestamp.txt'
-JSON_FILE = 'https://raw.githubusercontent.com/PokeMiners/game_masters/master/latest/latest.json'
+GAME_MASTER_TYPE = list[dict[str, Any]]
+
+VERSION_FILE = "https://raw.githubusercontent.com/PokeMiners/game_masters/master/latest/timestamp.txt"
+JSON_FILE = "https://raw.githubusercontent.com/PokeMiners/game_masters/master/latest/latest.json"
+
 
 def update():
-    with open('version.txt', encoding='utf-8') as f:
+    with open("version.txt", encoding="utf-8") as f:
         try:
             local_version = int(f.readline())
         except ValueError:
             local_version = 0
 
-    print(f'Checking latest GAME_MASTER versions (Local version: {local_version})...')
+    print(f"Checking latest GAME_MASTER versions (Local version: {local_version})...")
 
     with urllib.request.urlopen(VERSION_FILE) as f:
         latest_version = int(f.read())
 
     if latest_version <= local_version:
-        print('Local version is the latest version, no update needed.')
+        print("Local version is the latest version, no update needed.")
         return
 
-    print(f'Found newer version of GAME_MASTER file (Latest: {latest_version}).')
+    print(f"Found newer version of GAME_MASTER file (Latest: {latest_version}).")
 
-    print('Downloading latest version of GAME_MASTER file...')
+    print("Downloading latest version of GAME_MASTER file...")
     with urllib.request.urlopen(JSON_FILE) as f:
         latest = f.read()
 
-    with open('data.json', 'w', encoding='utf-8') as f:
+    with open("data.json", "w", encoding="utf-8") as f:
         f.write(latest.decode())
-    with open('version.txt', 'w', encoding='utf-8') as f:
+    with open("version.txt", "w", encoding="utf-8") as f:
         f.write(str(latest_version))
 
-    print('GAME_MASTER file successfully updated.')
+    print("GAME_MASTER file successfully updated.")
 
 
-def get_json():
+def get_json() -> GAME_MASTER_TYPE:
     update()
 
-    with open('data.json', encoding='utf-8') as data_file:
+    with open("data.json", encoding="utf-8") as data_file:
         data = json.load(data_file)
     return data
--- pokemon.py
+++ pokemon.py
@@ -1,122 +1,187 @@
 import os
+from typing import Any
+
 import game_master
 
 
-def write_csv(path, header, data):
-    with open(os.path.join(os.getcwd(), path), 'w', encoding='utf-8') as fi:
-        fi.write(','.join(header))
-        fi.write('\n')
-        fi.write('\n'.join(data))
+def write_csv(path: str, header: list[str], data: list[str]):
+    with open(os.path.join(os.getcwd(), path), "w", encoding="utf-8") as fi:
+        fi.write(",".join(header))
+        fi.write("\n")
+        fi.write("\n".join(data))
 
 
-def handle_pokemon(data):
-    pokemons = {}
+def handle_pokemon(data: game_master.GAME_MASTER_TYPE):
+    pokemons: dict[str, Any] = {}
     for template in data:
-        if 'pokemonSettings' in template['data']:
-            pokemonId = str(int(template['templateId'][1:5]))
-            pokemon = template['data']['pokemonSettings']
+        if "pokemonSettings" in template["data"]:
+            pokemonId = str(int(template["templateId"][1:5]))
+            pokemon: dict[str, Any] = template["data"]["pokemonSettings"]
             if pokemonId not in pokemons:
+                stats: dict[str, int] = pokemon["stats"]
+                encounter: dict[str, int] = pokemon["encounter"]
                 pokemons[pokemonId] = {
-                    'Name': pokemon['pokemonId'],
-                    'Form': [str(pokemon['form'])] if 'form' in pokemon else [],
-                    'Base Stamina': pokemon['stats'].get('baseStamina', 0),
-                    'Base Attack': pokemon['stats'].get('baseAttack', 0),
-                    'Base Defence': pokemon['stats'].get('baseDefense', 0),
-                    'Type1': pokemon['type'],
-                    'Type2': pokemon.get('type2', 'POKEMON_TYPE_NONE'),
-                    'Base Capture Rate': pokemon['encounter'].get('baseCaptureRate', 0),
-                    'Base Flee Rate': pokemon['encounter'].get('baseFleeRate', 0),
-                    'Height (m)': pokemon.get('pokedexHeightM', 0),
-                    'Height SD': pokemon.get('heightStdDev', 0),
-                    'Weight (kg)': pokemon.get('pokedexWeightKg', 0),
-                    'Weight SD': pokemon.get('weightStdDev', 0),
-                    'Candy To Evolve': pokemon.get('candyToEvolve', 0),
-                    'Buddy Candy Distance (km)': pokemon['kmBuddyDistance'],
-                    'Model Height': pokemon.get('modelHeight', 0),
-                    'Buddy Size': pokemon.get('buddySize', 'BUDDY_NORMAL'),
-                    'Quick Moves': pokemon.get('quickMoves', []),
-                    'Cinematic Moves': pokemon.get('cinematicMoves', []),
+                    "Name": pokemon["pokemonId"],
+                    "Form": [str(pokemon["form"])] if "form" in pokemon else [],
+                    "Base Stamina": stats.get("baseStamina", 0),
+                    "Base Attack": stats.get("baseAttack", 0),
+                    "Base Defence": stats.get("baseDefense", 0),
+                    "Type1": pokemon["type"],
+                    "Type2": pokemon.get("type2", "POKEMON_TYPE_NONE"),
+                    "Base Capture Rate": encounter.get("baseCaptureRate", 0),
+                    "Base Flee Rate": encounter.get("baseFleeRate", 0),
+                    "Height (m)": pokemon.get("pokedexHeightM", 0),
+                    "Height SD": pokemon.get("heightStdDev", 0),
+                    "Weight (kg)": pokemon.get("pokedexWeightKg", 0),
+                    "Weight SD": pokemon.get("weightStdDev", 0),
+                    "Candy To Evolve": pokemon.get("candyToEvolve", 0),
+                    "Buddy Candy Distance (km)": pokemon["kmBuddyDistance"],
+                    "Model Height": pokemon.get("modelHeight", 0),
+                    "Buddy Size": pokemon.get("buddySize", "BUDDY_NORMAL"),
+                    "Quick Moves": pokemon.get("quickMoves", []),
+                    "Cinematic Moves": pokemon.get("cinematicMoves", []),
                 }
-            elif 'form' in pokemon:
-                pokemons[pokemonId].setdefault('Form', []).append(str(pokemon['form']))
+            elif "form" in pokemon:
+                pokemons[pokemonId].setdefault("Form", []).append(str(pokemon["form"]))
 
-            for evolution in pokemon.get('evolutionBranch', []):
-                if 'evolution' in evolution or 'temporaryEvolution' in evolution:
-                    evolve = evolution.get('evolution', evolution.get('temporaryEvolution'))
-                    form = pokemon.get('form', f'{pokemon["pokemonId"]}_NORMAL')
-                    pokemons[pokemonId].setdefault('Evolutions', {})[form] = evolution
+            for evolution in pokemon.get("evolutionBranch", []):
+                if "evolution" in evolution or "temporaryEvolution" in evolution:
+                    evolve = evolution.get(
+                        "evolution", evolution.get("temporaryEvolution")
+                    )
+                    form = pokemon.get("form", f'{pokemon["pokemonId"]}_NORMAL')
+                    pokemons[pokemonId].setdefault("Evolutions", {})[form] = evolution
 
-    pokemon_columns = ['ID', 'Name', 'Form', 'Base Stamina', 'Base Attack', 'Base Defence',
-                       'Type1', 'Type2', 'Base Capture Rate', 'Base Flee Rate',
-                       'Height (m)', 'Height SD', 'Weight (kg)', 'Weight SD',
-                       'Candy To Evolve', 'Buddy Candy Distance (km)', 'Model Height', 'Buddy Size']
-    move_columns = ['ID', 'Name', 'Quick Move', 'Cinematic Move']
-    evolution_columns = ['ID', 'Name', 'Form', 'Evolution', 'Candy']
-    pokemon_rows = []
-    move_rows = []
-    evolution_rows = []
+    pokemon_columns = [
+        "ID",
+        "Name",
+        "Form",
+        "Base Stamina",
+        "Base Attack",
+        "Base Defence",
+        "Type1",
+        "Type2",
+        "Base Capture Rate",
+        "Base Flee Rate",
+        "Height (m)",
+        "Height SD",
+        "Weight (kg)",
+        "Weight SD",
+        "Candy To Evolve",
+        "Buddy Candy Distance (km)",
+        "Model Height",
+        "Buddy Size",
+    ]
+    move_columns = ["ID", "Name", "Quick Move", "Cinematic Move"]
+    evolution_columns = ["ID", "Name", "Form", "Evolution", "Candy"]
+    pokemon_rows: list[str] = []
+    move_rows: list[str] = []
+    evolution_rows: list[str] = []
     for pokemonId, pokemon in pokemons.items():
-        pokemon['Form'] = '/'.join(pokemon['Form'])
-        row = f'{pokemonId},'
-        row += ','.join([str(pokemon[col]) for col in pokemon_columns[1:]])
+        pokemon["Form"] = "/".join(pokemon["Form"])
+        row = f"{pokemonId},"
+        row += ",".join([str(pokemon[col]) for col in pokemon_columns[1:]])
         pokemon_rows.append(row)
 
-        for quick_move in pokemon['Quick Moves']:
-            for cinematic_move in pokemon['Cinematic Moves']:
-                move_rows.append(','.join([pokemonId, pokemon['Name'], quick_move, cinematic_move]))
+        for quick_move in pokemon["Quick Moves"]:
+            for cinematic_move in pokemon["Cinematic Moves"]:
+                move_rows.append(
+                    ",".join(
+                        [
+                            pokemonId,
+                            str(pokemon["Name"]),
+                            quick_move,
+                            str(cinematic_move),
+                        ]
+                    )
+                )
 
-        for form, evolve in pokemon.get('Evolutions', {}).items():
-            if 'evolution' in evolve:
-                row = [pokemonId, pokemon['Name'], form, evolve['evolution'], evolve.get('candyCost', 0)]
+        for form, evolve in pokemon.get("Evolutions", {}).items():
+            if "evolution" in evolve:
+                row = [
+                    pokemonId,
+                    pokemon["Name"],
+                    form,
+                    evolve["evolution"],
+                    evolve.get("candyCost", 0),
+                ]
             else:
-                row = [pokemonId, pokemon['Name'], form, evolve['temporaryEvolution'], 0]
-            evolution_rows.append(','.join([str(i) for i in row]))
+                row = [
+                    pokemonId,
+                    pokemon["Name"],
+                    form,
+                    evolve["temporaryEvolution"],
+                    0,
+                ]
+            evolution_rows.append(",".join([str(i) for i in row]))
 
-    write_csv('pokemon.csv', pokemon_columns, pokemon_rows)
-    write_csv('pokemon_moves.csv', move_columns, move_rows)
-    write_csv('evolution.csv', evolution_columns, evolution_rows)
+    write_csv("pokemon.csv", pokemon_columns, pokemon_rows)
+    write_csv("pokemon_moves.csv", move_columns, move_rows)
+    write_csv("evolution.csv", evolution_columns, evolution_rows)
 
-    print('Done handling Pokemons.')
+    print("Done handling Pokemons.")
 
 
-def handle_move(data):
-    quick_columns = ['ID', 'Name', 'Pokemon Type', 'Power', 'Energy Gain',
-                     'Stamina Loss', 'Duration (ms)', 'Window Start (ms)', 'Window End (ms)',
-                     'Accuracy', 'Critical']
+def handle_move(data: game_master.GAME_MASTER_TYPE):
+    quick_columns = [
+        "ID",
+        "Name",
+        "Pokemon Type",
+        "Power",
+        "Energy Gain",
+        "Stamina Loss",
+        "Duration (ms)",
+        "Window Start (ms)",
+        "Window End (ms)",
+        "Accuracy",
+        "Critical",
+    ]
     cinematic_columns = quick_columns.copy()
-    cinematic_columns[4] = 'Energy Used'
-    quick_rows = []
-    cinematic_rows = []
+    cinematic_columns[4] = "Energy Used"
+    quick_rows: list[str] = []
+    cinematic_rows: list[str] = []
     for template in data:
-        if 'moveSettings' in template['data']:
-            move = template['data']['moveSettings']
-            row = [int(template['templateId'][1:5]), move['movementId'], move['pokemonType'],
-                   move.get('power', 0), move.get('energyDelta', 0),
-                   move.get('staminaLossScalar', 0), move['durationMs'], move['damageWindowStartMs'],
-                   move['damageWindowEndMs'], move['accuracyChance'], move.get('criticalChance', 0)]
+        if "moveSettings" in template["data"]:
+            move = template["data"]["moveSettings"]
+            if len(template["templateId"].split("_")[0]) != 5:
+                # placeholder data wihtout id
+                continue
+            row = [
+                int(template["templateId"][1:5]),
+                move["movementId"],
+                move["pokemonType"],
+                move.get("power", 0),
+                move.get("energyDelta", 0),
+                move.get("staminaLossScalar", 0),
+                move["durationMs"],
+                move.get("damageWindowStartMs", 0),
+                move.get("damageWindowEndMs", 0),
+                move["accuracyChance"],
+                move.get("criticalChance", 0),
+            ]
 
-            if 'FAST' in str(move['movementId']):
-                quick_rows.append(','.join([str(i) for i in row]))
+            if "FAST" in str(move["movementId"]):
+                quick_rows.append(",".join([str(i) for i in row]))
             else:
                 row[4] = abs(row[4])
-                cinematic_rows.append(','.join([str(i) for i in row]))
+                cinematic_rows.append(",".join([str(i) for i in row]))
 
-    write_csv('quick_moves.csv', quick_columns, quick_rows)
-    write_csv('cinematic_moves.csv', cinematic_columns, cinematic_rows)
+    write_csv("quick_moves.csv", quick_columns, quick_rows)
+    write_csv("cinematic_moves.csv", cinematic_columns, cinematic_rows)
 
-    print('Done handling moves.')
+    print("Done handling moves.")
 
 
 def main():
     data = game_master.get_json()
 
-    print('Processing GAME_MASTER data...')
+    print("Processing GAME_MASTER data...")
 
     handle_pokemon(data)
     handle_move(data)
 
-    print('All finished.')
+    print("All finished.")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
